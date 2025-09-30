393fc22c89abef4cfe14e37163e6ecc995fd33cf
ankitpazare222 <ankitpazare222@gmail.com>
2025-09-30 21:13:22 +0530

    Delete examportal directory

--- examportal/src/app/sevices/admin.guard.ts
+++
@@ -1,8 +0,0 @@
-import { inject } from '@angular/core';
-import { CanActivateFn } from '@angular/router';
-import { LoginuserService } from './loginuser.service';
-
-export const adminGuard: CanActivateFn = (route, state) => {
-  
-  return true;
-};
